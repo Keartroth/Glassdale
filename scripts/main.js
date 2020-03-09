416b883a6b27eb0d58ca4c142a505135b670bf5a
Keartroth <mecarrolljr@gmail.com>
2020-03-09 14:33:23 -0500

    added a list of crimes dropdown menue item to later filter by crimes.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 import { getCriminals } from "./criminals/CriminalProvider.js";
 import { criminalList } from "./criminals/CriminalList.js";
+import ConvictionSelect from "./convictions/ConvictionsSelect.js";
+import { getConvictions } from "./convictions/ConvictionsProvider.js";
 
 getCriminals().then(criminalList);
+getConvictions().then(ConvictionSelect);
