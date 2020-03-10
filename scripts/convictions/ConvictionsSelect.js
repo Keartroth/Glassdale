52e96356d650ed19c3132c9c64b932860c09f8c4
Keartroth <mecarrolljr@gmail.com>
2020-03-09 20:58:50 -0500

    adding eventHub listeners to filter criminals by crimes commited.

--- scripts/convictions/ConvictionsSelect.js
+++ scripts/convictions/ConvictionsSelect.js
@@ -1,28 +1,77 @@
+// /*
+//  *   ConvictionSelect component that renders a select HTML element
+//  *   which lists all convictions in the Glassdale PD API
+//  */
+// import { useConvictions } from "./ConvictionsProvider.js";
+
+// const contentTargetElement = document.querySelector(".filters__crime");
+
+// const ConvictionSelect = () => {
+//      const convictions = useConvictions();
+
+//     const render = convictionsCollection => {
+//            contentTargetElement.innerHTML = `
+//                <select class="dropdown" id="crimeSelect">
+//                    <option value="0">Please select a crime...</option>
+//                    ${
+//                     convictionsCollection.map(crime => {
+//                            return `<option id="crime--${crime.id}" class="crimesMenueItem">${crime.name}</option>`
+//                        })
+//                    }
+//                </select>
+//            `
+//     }
+
+//     render(convictions)
+// }
+
+// export default ConvictionSelect;
+// -------------------------------------------------------------NEW CODE BELOW HERE--------------------------------------------------------------------
+
 /*
  *   ConvictionSelect component that renders a select HTML element
  *   which lists all convictions in the Glassdale PD API
  */
 import { useConvictions } from "./ConvictionsProvider.js";
 
+/*
+    Which element in your HTML contains all components?
+    That's your Event Hub. Get a reference to it here.
+*/
+const eventHub = document.querySelector(".container");
 const contentTargetElement = document.querySelector(".filters__crime");
 
-const ConvictionSelect = () => {
-     const convictions = useConvictions();
-
-    const render = convictionsCollection => {
-           contentTargetElement.innerHTML = `
-               <select class="dropdown" id="crimeSelect">
-                   <option value="0">Please select a crime...</option>
-                   ${
-                    convictionsCollection.map(crime => {
-                           return `<option id="crime--${crime.id}" class="crimesMenueItem">${crime.name}</option>`
-                       })
-                   }
-               </select>
-           `
+// On the content target, listen for a "change" event.
+contentTargetElement.addEventListener(
+    "change", 
+    event => {
+    // Only do this if the `crimeSelect` element was changed
+    if (event.target.value !== 0) {
+        // Create custom event. Provide an appropriate name.
+        let changeConvictionEvent = new CustomEvent(event);
+        // Dispatch to event hub
+        eventHub.dispatchEvent(changeConvictionEvent);
     }
+})
 
-    render(convictions)
+
+const render = convictionsCollection => {
+    contentTargetElement.innerHTML = `
+        <select class="dropdown" id="crimeSelect">
+            <option value="0">Please select a crime...</option>
+            ${
+                convictionsCollection.map(crime => {
+                       return `<option id="crimeId--${crime.id}" class="crimesMenueItem">${crime.name}</option>`
+                   })
+               }
+        </select>
+    `;
+}
+
+
+const ConvictionSelect = () => {
+    const convictions = useConvictions();
+    render(convictions);
 }
 
 export default ConvictionSelect;
--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,12 +1,47 @@
 import { useCriminals } from "./CriminalProvider.js";
 import { criminal } from "./Criminal.js";
 
+// const targetContentElement = document.querySelector(".criminalsContainer");
+
+// export const criminalList = () => {
+//     const arrayOfCriminalObjects = useCriminals();
+
+//     for (const criminalObject of arrayOfCriminalObjects) {
+//         targetContentElement.innerHTML += criminal(criminalObject);
+//     }
+// }
+
 const targetContentElement = document.querySelector(".criminalsContainer");
+const eventHub = document.querySelector(".container");
 
+// Listen for the custom event you dispatched in ConvictionSelect
+eventHub.addEventListener('changeConvictionEvent', event => {
+    // You remembered to add the id of the crime to the event detail, right?
+    if ("crimeId" in event.detail) {
+        /*
+            Filter the criminals application state down to the people that committed the crime
+        */
+        const matchingCriminals = appStateCriminals.filter(currentCriminals => {
+            return currentCriminals.id = true;
+        })
+
+        /*
+        Then invoke render() and pass the filtered collection as
+        an argument
+         */
+       render(matchingCriminals);
+    }
+})
+
+const render = criminalCollection => {
+    targetContentElement.innerHTML += criminal(criminalCollection);
+}
+
+
+// Render ALL criminals initally
 export const criminalList = () => {
-    const arrayOfCriminalObjects = useCriminals();
-
-    for (const criminalObject of arrayOfCriminalObjects) {
-        targetContentElement.innerHTML += criminal(criminalObject);
+    const appStateCriminals = useCriminals();
+    for (const criminalObject of appStateCriminals) {
+        render(criminalObject);
     }
 }
