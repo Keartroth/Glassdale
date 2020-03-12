1a06ac35c88b409980d372725d23ae4bf35a8505
Keartroth <mecarrolljr@gmail.com>
2020-03-11 23:18:29 -0500

    attempting to acquire criminal unique id number from criminal array with .map and .find using criminal's name when entered on a form when submitting a note. work in progress.

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,4 +1,5 @@
 import { saveNote } from "./noteDataProvider.js"
+import { useCriminals } from "../criminals/CriminalProvider.js"
 
 const contentTargetElement = document.querySelector(".noteFormContainer")
 /*
@@ -23,20 +24,38 @@ const render = () => {
  *  Adds a "click" event listener to the button element (#saveNote) that collects the user entered data 
  *  of the form element (#noteForm) and runs the function saveNote to submit the data. 
 */
+
 contentTargetElement.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "saveNote") {
+        const criminals = useCriminals();
         const contentTargetDate = document.getElementById("note--date").value;
         const contentTargetSuspect = document.getElementById("note--suspect").value;
         const contentTargetNoteText = document.getElementById("note--text").value;
 
+        const findCriminalID = (arrayOfCriminalObjects, criminalName) => {
+            const criminalIdValue = arrayOfCriminalObjects.find(
+                criminal => {
+                    criminal.name === criminalName.name;
+                    return criminal.id;
+                });
+            
+            return criminalIdValue
+        }
+
+        const relatedCriminalId = findCriminalID(criminals, contentTargetSuspect);
+        
         const newNote = {
             "date": contentTargetDate,
             "suspect": contentTargetSuspect,
-            "noteText": contentTargetNoteText
+            "noteText": contentTargetNoteText,
+            "criminalId": relatedCriminalId
         }
         saveNote(newNote)
     }
 })
+
+// arrayOfNoteObjects.map(note => {
+//     const relatedCriminal = arrayOfCriminalObjects.find(criminal => criminal.id === note.criminalId)
 
 // Renders a form to the DOM in the article element (.noteFormContainer) that is used to submit case notes.
 export const NoteForm = () => {
