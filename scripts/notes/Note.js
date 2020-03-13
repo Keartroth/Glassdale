1717ca2d7dc00fae3a316adb4f558418e60c00ca
Keartroth <mecarrolljr@gmail.com>
2020-03-12 22:05:05 -0500

    added an edit button and a skeleton of a dialog box to notes for future edit function.

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -3,6 +3,12 @@ export const NoteComponent = noteObject => `
         <h4 class="note__suspect"><span class="bold">Suspect</span>: ${noteObject.suspect}</h4>
         <p class="note__timestamp"><span class="bold">Note Entry Date</span>: ${noteObject.date}</p>
         <p class="note__text">${noteObject.noteText}</p>
+        <button id="editNote--${noteObject.id}" class="editNoteButton">Edit Note</button>
+        <dialog class="dialog--note" id="details--${noteObject.id}">
+        
+        <button id="editNoteSubmit--${noteObject.id}" class="editNoteSubmitButton">Submit Edited Note</button>
+        <button class="button--close" id="closeEdit-${noteObject.id}">Close Unedited</button>
+        </dialog>
         <button id="deleteNote--${noteObject.id}" class="deleteNoteButton">Delete Note</button>
     </section>
 `
